670c73c907f6b4483dcd51c2c47a2e3335b6c83e
ScottyKnows44 <scottcfalcon@gmail.com>
2022-11-15 20:04:54 -0800

    POST and PATCH

--- API/main.py
+++ API/main.py
@@ -1,15 +1,34 @@
 import requests
+import json
 
-api_url = "https://jsonplaceholder.typicode.com/todos/1"
+api_url = "https://jsonplaceholder.typicode.com/todos"
+
+# Get Request
 response = requests.get(api_url)
+
 print(response.json())  # {'userId': 1, 'id': 1, 'title': 'delectus aut autem', 'completed': False}
+
 print(response.status_code)  # 200
 print(response.headers["Content-Type"])  # application/json; charset=utf-8
 
-todo = {"userId":1, "title": "Buy milk", "completed": False}
+
+# POST request
+todo = {"userId": 1, "title": "Buy milk", "completed": False}
+
 response = requests.post(api_url, json=todo)
-response.json() # {'userId': 1, 'title': 'Buy milk', 'completed': False, 'id': 201}
-response.status_code # 200
+
+print(response.json()) # {'userId': 1, 'title': 'Buy milk', 'completed': False, 'id': 201}
+
+print(response.status_code) # 200
+
+
+# Put Request
+todo = {"title": "Mow lawn"}
+response = requests.put(api_url, json=todo)
+print(response.json)
+print(response.status_code)
+
+
 
 
 
